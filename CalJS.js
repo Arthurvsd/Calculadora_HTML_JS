147a688e34975c6b67dd9a0181e24a2f7204a704
Arthur Vinícius <arthurvsd2005@gmail.com>
2023-08-05 23:28:28 -0300

    Calculadora em HMTL e JS
    Aula antiga do professor Fhelipe, onde fizemos uma calculadora.

---
+++ CalJS.js
@@ -0,0 +1,31 @@
+function somar(){
+ 
+    let v1 = document.getElementById('valor1').value;
+    let v2 = document.getElementById('valor2').value;
+    let resultado = parseInt(v1)+parseInt(v2);
+    alert(resultado);
+}
+
+function subtrair(){
+ 
+    let v1 = document.getElementById('valor1').value;
+    let v2 = document.getElementById('valor2').value;
+    let resultado = parseInt(v1)-parseInt(v2);
+    alert(resultado);
+}
+
+function multiplicar(){
+ 
+    let v1 = document.getElementById('valor1').value;
+    let v2 = document.getElementById('valor2').value;
+    let resultado = parseInt(v1)*parseInt(v2);
+    alert(resultado);
+}
+
+function dividir(){
+ 
+    let v1 = document.getElementById('valor1').value;
+    let v2 = document.getElementById('valor2').value;
+    let resultado = parseInt(v1)/parseInt(v2);
+    alert(resultado);
+}
